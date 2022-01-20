e70df1a90f4404071decd3c079ca2b8a92447e2e
Patrick Cook <masonobledev@gmail.com>
2022-01-20 17:30:40 -0500

    dogIndex additions

--- src/Components/Dogs/DogIndex.js
+++ src/Components/Dogs/DogIndex.js
@@ -18,6 +18,29 @@ class DogIndex extends React.Component {
                 isLoaded: true,
                 photos: dogPhotos,
             })
-        })
+        });
     }
-}
+
+    render() {
+        return (
+            <div>
+                <h1>My Component has mounted, Check the browser 'console'</h1>
+                {
+                    this.photos.length === 0
+                    ? 'Loading dog pics...'
+                    : this.state.photos.map((photo) => (
+                        <figure key={photo.id}>
+                        <img src={this.photo.avatar} />
+                        {{photo.avatar}}
+                        <figcaption>
+                        {photo.name}
+                        </figcaption>
+                        </figure>
+                        ))
+                }
+            </div>
+        );
+    }
+};
+
+export default DogIndex;
